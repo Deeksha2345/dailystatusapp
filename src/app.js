c456195eb575be549bd33145977f2a85d6ca3f90
Deeksha2345 <104678229+Deeksha2345@users.noreply.github.com>
2022-06-29 17:49:32 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// var MongoClient = require('mongodb').MongoClient
+ var MongoClient = require('mongodb').MongoClient
 //Create a database named "mydb":
 // var url = "mongodb://localhost:27017/mydatabase1";
 
